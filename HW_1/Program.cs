96b6a3986acf5cb3d6d2cdb85c7ea920e046cb40
Feruz <feruz.muradoff.93@mail.ru>
2023-06-11 09:40:33 +0500

    Исправлена ошибка

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -8,7 +8,7 @@ int num1 = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число: ");
 int num2 = int.Parse(Console.ReadLine()!);
 int max = num1;
-if(num2>max);
+if(num2>max)
 {
     max = num2;
 }
